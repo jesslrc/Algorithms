c18431bb17c5cef5c176f3e84144294343be67f1
jesslrc <97973102+jesslrc@users.noreply.github.com>
2023-10-16 15:13:55 -0600

    Two Sum commit

---
+++ src/main/java/org/jesslrc/TwoSum.java
@@ -0,0 +1,27 @@
+package org.jesslrc;
+
+public class TwoSum {
+    public static int[] twoSum(int[] nums, int target) {
+        int[] num = new int[2];
+
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    num[0] = i;
+                    num[1] = j;
+                }
+            }
+        }
+        return num;
+    }
+    public static void main(String[] args) {
+
+        int[] nums = {2, 7, 11, 15};
+        int target = 18;
+
+        int[] result = twoSum(nums, target);
+        for (int j : result) {
+            System.out.println("Index: " + j);
+        }
+    }
+}
